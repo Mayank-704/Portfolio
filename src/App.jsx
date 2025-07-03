91ce0a5f93f6e32b3420f63eb044bf274d8116fb
Mayank Gupta <mayankg88813@gmail.com>
2025-07-03 21:34:15 +0530

    some ui changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import Hero from "./sections/Hero.jsx"
 import About from './sections/About.jsx'
 import Projects from './sections/Projects.jsx'
 import Footer from './sections/Footer.jsx'
+// import Contact from './sections/Contact.jsx'
 function App() {
   return (
     <div className='container mx-auto max-w-7xl'>
-      {/* <Navbar/> */}
+      <Navbar/>
       <Hero/>
       <About/>
       <Projects/>
+      {/* <Contact/> */}
       <Footer/>
       {/* <section className='h-screen' ></section> */}
     </div>
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -4,7 +4,7 @@ import { FaLinkedin } from "react-icons/fa";
 import { FaWhatsapp } from "react-icons/fa";
 const Footer = () => {
   return (
-    <section className="flex flex-wrap items-center justify-between gap-5 pb-3 text-sm text-neutral-400 c-space h-[20vh]">
+    <section className="flex flex-wrap items-center justify-center md:justify-between gap-5 pb-3 text-sm text-neutral-400 c-space h-[20vh]">
       <div className="mb-4 bg-gradient-to-r from-transparent via-neutral-700 to-transparent h-[1px] w-full" />
       <div className="flex gap-2">
         <p>Terms & Conditions</p>
@@ -18,9 +18,9 @@ const Footer = () => {
           <a href="">
             <FaLinkedin size={24} className="hover:scale-130"/>
           </a>
-          <a href="">
+          {/* <a href="">
             <FaWhatsapp size={24} className="hover:scale-130"/>
-          </a>
+          </a> */}
 
       </div>
       <p>© 2025 Mayank. Portfolio inspired by <a className="cursor-pointer" href="https://www.linkedin.com/in/ali-sanati/">Ali Sanati</a></p>

a85b773d46a7bdf7c1168a91f9d81f7cce60c9df
Mayank Gupta <mayankg88813@gmail.com>
2025-10-25 10:42:57 +0530

    Add Experiences section and implement Timeline component; restructure project constants

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Hero from "./sections/Hero.jsx"
 import About from './sections/About.jsx'
 import Projects from './sections/Projects.jsx'
 import Footer from './sections/Footer.jsx'
+import Experiences from "./sections/Experiences.jsx";
+
 // import Contact from './sections/Contact.jsx'
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
       <Hero/>
       <About/>
       <Projects />
+      <Experiences />
       {/* <Contact/> */}
       <Footer/>
       {/* <section className='h-screen' ></section> */}

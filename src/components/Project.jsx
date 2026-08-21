d001a08bcca9d72252abd0238446799f8c07ae45
Mayank Gupta <mayankg88813@gmail.com>
2026-08-21 08:35:39 +0530

    Update: Remove old project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -45,42 +45,6 @@ const content = [
   {
     title: (
        <div className="flex items-center">
-        Prism 2.0
-        <a
-          href="https://www.prism2025.tech/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="ml-2 underline text-blue-300"
-        >
-         <LuSquareArrowOutUpRight />
-        </a>
-      </div>
-    ),
-    description: (
-      <>
-        Developed 'PRISM 2.0', the official website for our college's tech fest, with event details and registration features.{" "}
-        <br />
-        <img src="/assets/logos/react.svg" alt="React" className="inline h-5 ml-1" />
-        <img src="/assets/logos/tailwindcss.svg" alt="TailwindCSS" className="inline h-5 ml-1" />
-        <img src="/assets/logos/expressjsWhite.svg" alt="Express" className="inline h-5 ml-1" />
-        <img src="/assets/logos/postgresql.svg" alt="postgreSQL" className="inline h-5 ml-1" />
-      </>
-    ),
-    content: (
-      <>
-        <img
-          src="/assets/projects/prism2.0.png"
-          width={300}
-          height={200}
-          className="h-full w-full object-fill mb-4"
-          alt="Prism 2.0 project"
-        />
-      </>
-    ),
-  },
-  {
-    title: (
-       <div className="flex items-center">
         Maidaan
         <a
           href="https://maidaan-extra-cool.vercel.app/"
